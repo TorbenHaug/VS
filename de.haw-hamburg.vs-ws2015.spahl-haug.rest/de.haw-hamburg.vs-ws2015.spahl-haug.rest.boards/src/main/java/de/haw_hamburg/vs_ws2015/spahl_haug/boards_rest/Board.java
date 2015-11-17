894bdcd1979ff85f298b5a4177336ab0541ae026
Isaisaisa <louisa@spahl-hamburg.de>
2015-11-17 11:05:06 +0100

    Add raw implementation of Boeard

--- de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.boards/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/boards_rest/Board.java
+++ de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.boards/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/boards_rest/Board.java
@@ -3,15 +3,19 @@ package de.haw_hamburg.vs_ws2015.spahl_haug.boards_rest;
 import de.haw_hamburg.vs_ws2015.spahl_haug.boards_rest.dto.GamesDTO;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Louisa on 17.11.2015.
  */
 public class Board {
 
-    private GamesDTO game;
+    private List<Field> fields;
+    private Map<Integer, Integer> positions;
 
     public Board() {
 
     }
+
+
 }
---
+++ de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.boards/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/boards_rest/Place.java
@@ -0,0 +1,8 @@
+package de.haw_hamburg.vs_ws2015.spahl_haug.boards_rest;
+
+/**
+ * Created by Louisa on 17.11.2015.
+ */
+public class Place {
+    private String name;
+}
