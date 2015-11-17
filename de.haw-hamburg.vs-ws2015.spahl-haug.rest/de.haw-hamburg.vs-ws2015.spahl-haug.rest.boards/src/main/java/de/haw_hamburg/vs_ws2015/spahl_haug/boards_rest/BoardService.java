0eac83cefd110cfeb34a6f78bbe5f3ec5f866165
Torben Haug <claustorbenhaug@hotmail.de>
2015-11-17 11:07:57 +0100

    map

--- de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.boards/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/boards_rest/BoardService.java
+++ de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.boards/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/boards_rest/BoardService.java
@@ -2,15 +2,17 @@ package de.haw_hamburg.vs_ws2015.spahl_haug.boards_rest;
 
 import de.haw_hamburg.vs_ws2015.spahl_haug.boards_rest.dto.GamesDTO;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Louisa on 17.11.2015.
  */
 public class BoardService {
 
-    private List<Board> boards;
+	private final Map<Integer, Board> boards = new HashMap<>();
 
-    public BoardService(){
-    }
+	public BoardService(){
+	}
 }
