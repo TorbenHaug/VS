8c0e5e241c1787d4031e812ba3f85fbae018caa8
Torben Haug <claustorbenhaug@hotmail.de>
2016-01-10 14:42:56 +0100

    fix bankdiscovery

--- de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.brocker/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/brocker_rest/BrockerService.java
+++ de.haw-hamburg.vs-ws2015.spahl-haug.rest/de.haw-hamburg.vs-ws2015.spahl-haug.rest.brocker/src/main/java/de/haw_hamburg/vs_ws2015/spahl_haug/brocker_rest/BrockerService.java
@@ -24,97 +24,97 @@ import de.haw_hamburg.vs_ws2015.spahl_haug.servicerepository.ServiceRepository;
 
 public class BrockerService {
 
-    private final Map<String, Brocker> brockers;
-    private String bankServiceURI = null;
-    private final RestTemplate restTemplate = new RestTemplate();
+	private final Map<String, Brocker> brockers;
+	private String bankServiceURI = null;
+	private final RestTemplate restTemplate = new RestTemplate();
 
-    public BrockerService(){
-        brockers = new ConcurrentHashMap<>();
-        //Uncommit for testing
-        //bankServiceURI = "http://192.168.99.100:4568/banks"
-    }
+	public BrockerService(){
+		brockers = new ConcurrentHashMap<>();
+		//Uncommit for testing
+		//bankServiceURI = "http://192.168.99.100:4568/banks"
+	}
 
-    private String getBankServiceURI() throws RepositoryException{
-        if(bankServiceURI==null){
-            final ServiceRepository serviceRepository = new ServiceRepository();
-            try {
-                bankServiceURI = serviceRepository.getService("bank");
-            } catch (final Exception e) {
-                throw new RepositoryException("Cannot find bank");
-            }
-        }
-        return bankServiceURI;
-    }
-    public Brocker getBrocker(final String gameId) throws BrockerNotExistsException {
-        final Brocker brocker = brockers.get(gameId);
-        if(brocker == null){
-            throw new BrockerNotExistsException("There's no brocker for game " + gameId);
-        }
-        return brocker;
+	private String getBankServiceURI() throws RepositoryException{
+		if(bankServiceURI==null){
+			final ServiceRepository serviceRepository = new ServiceRepository();
+			try {
+				bankServiceURI = serviceRepository.getService("spahl_haug_bank") + "/banks";
+			} catch (final Exception e) {
+				throw new RepositoryException("Cannot find bank");
+			}
+		}
+		return bankServiceURI;
+	}
+	public Brocker getBrocker(final String gameId) throws BrockerNotExistsException {
+		final Brocker brocker = brockers.get(gameId);
+		if(brocker == null){
+			throw new BrockerNotExistsException("There's no brocker for game " + gameId);
+		}
+		return brocker;
 
-    }
+	}
 
-    public Brocker createBrocker(final String gameId, final BrockerDTO brockerDTO) throws Exception {
-        if(brockers.containsKey(gameId)){
-            throw new Exception("Brocker already exists");
-        }
-        return brockers.put(gameId, new Brocker(gameId, brockerDTO));
+	public Brocker createBrocker(final String gameId, final BrockerDTO brockerDTO) throws Exception {
+		if(brockers.containsKey(gameId)){
+			throw new Exception("Brocker already exists");
+		}
+		return brockers.put(gameId, new Brocker(gameId, brockerDTO));
 
-    }
+	}
 
-    public void removeBrocker(final String gameId) {
-        brockers.remove(gameId);
+	public void removeBrocker(final String gameId) {
+		brockers.remove(gameId);
 
-    }
+	}
 
-    public List<Place> getAllPlaces(final String gameId) throws BrockerNotExistsException {
-        return getBrocker(gameId).getPlaces();
-    }
+	public List<Place> getAllPlaces(final String gameId) throws BrockerNotExistsException {
+		return getBrocker(gameId).getPlaces();
+	}
 
-    public Place getPlace(final String gameId, final String placeid) throws BrockerNotExistsException, PlaceNotFoundException {
-        final Place place = getBrocker(gameId).getPlace(placeid);
-        if(place == null){
-            throw new PlaceNotFoundException("Place " + placeid + " not found");
-        }
-        return place;
+	public Place getPlace(final String gameId, final String placeid) throws BrockerNotExistsException, PlaceNotFoundException {
+		final Place place = getBrocker(gameId).getPlace(placeid);
+		if(place == null){
+			throw new PlaceNotFoundException("Place " + placeid + " not found");
+		}
+		return place;
 
-    }
+	}
 
-    public void createPlace(final String gameId, final String placeid, final Place place) throws PlaceAlreadyExistsExeption, BrockerNotExistsException {
-        getBrocker(gameId).createPlace(placeid,place);
+	public void createPlace(final String gameId, final String placeid, final Place place) throws PlaceAlreadyExistsExeption, BrockerNotExistsException {
+		getBrocker(gameId).createPlace(placeid,place);
 
-    }
+	}
 
-    public Player getOwner(final String gameId, final String placeid) throws PlaceNotFoundException, BrockerNotExistsException, NotSoldException, PlayerDoesntExistsException {
-        return getBrocker(gameId).getOwner(placeid);
+	public Player getOwner(final String gameId, final String placeid) throws PlaceNotFoundException, BrockerNotExistsException, NotSoldException, PlayerDoesntExistsException {
+		return getBrocker(gameId).getOwner(placeid);
 
-    }
+	}
 
-    public void changeOwner(final String gameId, final String placeid, final Player player) throws PlaceNotFoundException, PlayerDoesntExistsException, BrockerNotExistsException {
-        getBrocker(gameId).changeOwner(placeid,player.getId());
+	public void changeOwner(final String gameId, final String placeid, final Player player) throws PlaceNotFoundException, PlayerDoesntExistsException, BrockerNotExistsException {
+		getBrocker(gameId).changeOwner(placeid,player.getId());
 
-    }
+	}
 
-    public void buyPlace(final String gameId, final String placeid, final Player player) throws BrockerNotExistsException, PlaceNotFoundException, PlayerDoesntExistsException, BankRejectedException, NotForSaleException, RestClientException, RepositoryException {
-        final Place place = getPlace(gameId, placeid);
-        if(place.getOwner() != null){
-            throw new NotForSaleException("The Owner is " + place.getOwner());
-        }
-        getBrocker(gameId).getPlayer(player.getId());
-        transferMoneyToBank(gameId, place.getValue(),player.getId(), "Buy a Street!");
-        changeOwner(gameId, placeid, player);
+	public void buyPlace(final String gameId, final String placeid, final Player player) throws BrockerNotExistsException, PlaceNotFoundException, PlayerDoesntExistsException, BankRejectedException, NotForSaleException, RestClientException, RepositoryException {
+		final Place place = getPlace(gameId, placeid);
+		if(place.getOwner() != null){
+			throw new NotForSaleException("The Owner is " + place.getOwner());
+		}
+		getBrocker(gameId).getPlayer(player.getId());
+		transferMoneyToBank(gameId, place.getValue(),player.getId(), "Buy a Street!");
+		changeOwner(gameId, placeid, player);
 
-    }
+	}
 
-    public void hypothecaryCredit(final String gameId, final String placeid) {
-        throw new RuntimeException("Not yet Implemented");
+	public void hypothecaryCredit(final String gameId, final String placeid) {
+		throw new RuntimeException("Not yet Implemented");
 
-    }
+	}
 
-    public void deleteHypothecaryCredit(final String gameId, final String placeid) {
-        throw new RuntimeException("Not yet Implemented");
+	public void deleteHypothecaryCredit(final String gameId, final String placeid) {
+		throw new RuntimeException("Not yet Implemented");
 
-    }
+	}
 
 	public void visit(final String gameId, final String placeid, final String playerid) throws BrockerNotExistsException, PlaceNotFoundException, PlayerDoesntExistsException, BankRejectedException, RestClientException, RepositoryException {
 		final Place place = getPlace(gameId, placeid);
@@ -127,21 +127,21 @@ public class BrockerService {
 
 
 
-    }
+	}
 
-    private void transferMoneyToBank(final String gameId, final int value, final String playerId, final String reason) throws BankRejectedException, RestClientException, RepositoryException {
-        final ResponseEntity<String> transfer = restTemplate.postForEntity(getBankServiceURI() + "/" + gameId + "/transfer/from/" + playerId + "/" + value, reason, String.class);
-        if(transfer.getStatusCode() != HttpStatus.CREATED){
-            throw new BankRejectedException("Bank");
-        }
+	private void transferMoneyToBank(final String gameId, final int value, final String playerId, final String reason) throws BankRejectedException, RestClientException, RepositoryException {
+		final ResponseEntity<String> transfer = restTemplate.postForEntity(getBankServiceURI() + "/" + gameId + "/transfer/from/" + playerId + "/" + value, reason, String.class);
+		if(transfer.getStatusCode() != HttpStatus.CREATED){
+			throw new BankRejectedException("Bank");
+		}
 
-    }
-    private void transferMoneyFromPlayerToPlayer(final String gameId, final int value, final String playerIdFrom, final String playerIdTo, final String reason) throws BankRejectedException, RestClientException, RepositoryException {
-        final ResponseEntity<String> transfer = restTemplate.postForEntity(getBankServiceURI() + "/" + gameId + "/transfer/from/" + playerIdFrom + "/to/" + playerIdTo + "/" + value, reason, String.class);
-        if(transfer.getStatusCode() != HttpStatus.CREATED){
-            throw new BankRejectedException("Bank");
-        }
+	}
+	private void transferMoneyFromPlayerToPlayer(final String gameId, final int value, final String playerIdFrom, final String playerIdTo, final String reason) throws BankRejectedException, RestClientException, RepositoryException {
+		final ResponseEntity<String> transfer = restTemplate.postForEntity(getBankServiceURI() + "/" + gameId + "/transfer/from/" + playerIdFrom + "/to/" + playerIdTo + "/" + value, reason, String.class);
+		if(transfer.getStatusCode() != HttpStatus.CREATED){
+			throw new BankRejectedException("Bank");
+		}
 
-    }
+	}
 
 }
